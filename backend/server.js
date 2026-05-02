d539540c0593a1d7f83f87c88daa69a10837eebc
Chirag pratap <chiragpratap0009@gmail.com>
2026-05-02 12:53:55 +0530

    Fix Render port binding

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,11 @@ const authRoutes = require('./routes/authRoutes');
 const projectRoutes = require('./routes/projectRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
-const app = express();
+ 
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 // Connect Database
 connectDB();
